b072862b5db10de745c6302246c7d9adf4b0b20f
Pombinero <t4fany1236@gmail.com>
2024-01-14 14:27:40 -0600

    Adding js

---
+++ a.js
@@ -0,0 +1,10 @@
+//Tiffany Johan Jan.14 2024
+
+var math = 3;
+
+math++;
+
+var valorant = prompt ("Do you like valorant?");
+
+console.log("The variable name is:\n\n" + valorant + 
+    "\n\nThe value is: \n \n" + math);
