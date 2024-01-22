894e9262715a71f8815b76272e344117ac870bf6
Pombinero <t4fany1236@gmail.com>
2024-01-21 19:00:36 -0600

    loops

--- a.js
+++ a.js
@@ -1,10 +1,9 @@
-//Tiffany Johan Jan.14 2024
+var f = 5;
+var q = 0;
 
-var math = 3;
-
-math++;
-
-var valorant = prompt ("Do you like valorant?");
-
-console.log("The variable name is:\n\n" + valorant + 
-    "\n\nThe value is: \n \n" + math);
+while (q <= f)
+{
+    console.log(q)
+    q++;
+}
+ document.write(q);
