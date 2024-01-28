6c68554d1b758c2fab482d5efba7656da7991139
Pombinero <t4fany1236@gmail.com>
2024-01-28 15:24:22 -0600

    arrays

---
+++ b.js
@@ -0,0 +1,25 @@
+//create an array with 4 values//
+let myArray = [21, 45, 91, 20];
+
+//one parameter that adds one//
+let Increase = function(num)
+{
+    console.log(num + 1);
+}
+
+//two parameters//
+function oddNumber (funcExpression, number)
+{
+    if (number % 2 === 0) {
+        funcExpression(number);
+    }
+    else
+    {
+console.log("The number is odd");
+    }
+}
+
+// For-in loop//
+for (let index in myArray) {
+    oddNumber(Increase, myarray[index]);
+}
